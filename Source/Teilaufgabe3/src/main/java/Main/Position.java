7c7f476ad0ee2a03d4832ba57dcb9b67efbcbe96
Margarita S <simkinarita@gmail.com>
2019-01-18 20:29:45 +0100

    Added documentation logics

--- Source/Teilaufgabe3/src/main/java/Main/Position.java
+++ Source/Teilaufgabe3/src/main/java/Main/Position.java
@@ -22,12 +22,13 @@ public class Position {
   public static Position fromHalfMapNode(HalfMapNode node) {
     return new Position(node.getX(), node.getY());
   }
-
+// translating a position to a halfmap conjucted down (each y coordinate is +4 for this halfMap)
   public Position translate(Position other) {
     return new Position(x + other.x, y + other.y);
   }
 
   @Override
+  // addtional check for Objects , if located on a same postion =>same object
   public boolean equals(Object o) {
     if (this == o) {
       return true;
@@ -45,6 +46,7 @@ public class Position {
   }
 
   @Override
+  //general practice for a hashcode
   public int hashCode() {
     int result = x;
     result = 31 * result + y;
